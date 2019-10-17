54b10df6c0156cf893fa59fdfa37f5f51fea7ba3
Mykhailo "Ninin" Velykelskyi <unibreakfast1@gmail.com>
2019-10-17 08:28:20 +0300

    tweak for default route

--- back/onrequest.js
+++ back/onrequest.js
@@ -5,21 +5,19 @@ module.exports = (req, resp) => {
   const head = headers => resp.writeHead(200, headers),
         end = by => 
           typeof by == 'string' || by instanceof Buffer? 
-          resp.end(by) : ( head({body: JSON.stringify(by)}), end('') ),
+            resp.end(by) : ( head({body: JSON.stringify(by)}), end('') ),
         type = val => head({ 'Content-Type': val }),
-      { url } = req
+        url = req.url.wo('/')
 
-  log("-Handling request: " + url)
+  log("Handling request: /" + url)
 
   try {
 
-    if (url == '/') return end(file('front/index.html'))
-
-    if (url.startsWith('/api/')) 
-      return use('./back/api')(req, resp, end, url.wo('/api/'))
+    if (url.startsWith('api/')) 
+      return use('./back/api')(req, resp, end, url.wo('api/'))
 
     {
-      const content = file('front' + url)
+      const content = file('front/' + (url || 'index.html'))
       if (url.endsWith('.css')) type('text/css')
       if (url.endsWith('.js')) type('text/javascript')
       end(content)
@@ -27,7 +25,7 @@ module.exports = (req, resp) => {
   }
   catch (err) {
     if (err) log(err)
-    end('Sorry, ' + url + ' route unhandled or file not found, '
+    end('Sorry, /' + url + ' route unhandled or file not found, '
       + 'or cannot be provided right now for some reason')
   }
 
