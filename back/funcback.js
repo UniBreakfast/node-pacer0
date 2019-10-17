38ac551ea2f7af2e3cf519dd996cc25c3b4d4a9f
UniBreakfast <unibreakfast1@gmail.com>
2019-10-17 18:29:44 +0300

    pre feature cut

--- back/funcback.js
+++ back/funcback.js
@@ -1,9 +1,11 @@
+
 Object.defineProperty(global, 'now', 
   { get: ()=> String(new Date).match(/\d+:\d+:\d+/)[0], configurable: true } )
   
 log = (...args) => console.log(now, ...args) || args.pop()
 
 String.prototype.wo = function(piece) { return this.replace(piece, '') }
+
 
 use = module => setTimeout( ()=> {
   try { delete require.cache[require.resolve(dir+module)] } catch {}
--- index.js
+++ index.js
@@ -1,27 +1,19 @@
+
 dir = __dirname+'/'
 
 require('./back/funcback')
 
 imp(true, 'fs', 'http', 'mongodb')
 
-// setInterval(()=>{
-//   try {
-//     mongodb.MongoClient.connect(process.env.MONGO_URI, 
-//       {useNewUrlParser: true, useUnifiedTopology: true}, (err, cluster)=> {
-//         if (err) return log('hmm...', err)
-//         cluster.db('node-pacer0').collection('endeavors')
-//           .insertOne({now}, (err, result)=> log(err, result, '.'))
-//         cluster.close()
-//       })
-//   } catch (err) {log(err)}
-// }, 7e6)
 
 file = fs.readFileSync
 
 port = process.env.PORT || 3000
 
 dev = !process.env.PORT
+
 imp(!dev, './back/onrequest')
+
 
 http.createServer(onrequest).listen(port, 
   ()=> log('Server started to listen on port '+port))
