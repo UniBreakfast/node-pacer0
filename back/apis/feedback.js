d24dcb7a01756751b69bae877920c8a0fe46251e
UniBreakfast <unibreakfast1@gmail.com>
2019-10-18 23:03:12 +0300

    feedback echo api

---
+++ back/apis/feedback.js
@@ -0,0 +1,9 @@
+module.exports = (req, body, resp, end, url) => {
+
+  switch (url) {
+    case 'give':
+      return end(body)
+  }
+
+  throw ''
+}
--- front/script.js
+++ front/script.js
@@ -4,3 +4,10 @@ log('Page loaded')
 window.api = (route, body, cb=x=>x) => fetch('/api/'+route, 
   {method: 'POST', headers: {body: JSON.stringify(body)}})
     .then(resp => cb(JSON.parse([...resp.headers].find(([k])=>k=='body')[1])))
+
+submit.onclick = e => {
+  e.preventDefault()
+  api('feedback/give', {msg: msg.value, title: title.value},
+    answer => (msg.value = title.value = '', log(answer)))
+}
+
