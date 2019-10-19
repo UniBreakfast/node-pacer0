c0381a92ef22da57b305df05ab942c8dddad1a3a
UniBreakfast <unibreakfast1@gmail.com>
2019-10-19 21:49:51 +0300

    async await fetch rewrite

--- front/script.js
+++ front/script.js
@@ -1,18 +1,17 @@
 
 log('Page loaded')
 
-window.api = (route, body, cb=x=>x, errcb=log) => fetch('/api/'+route, 
-  {method: 'POST', headers: {body: JSON.stringify(body)}})
-    .then(resp => {
-      const body = [...resp.headers].find(([k])=>k=='body')
-      if (body) return body[1];  else throw resp
-    })
-    .then(body => cb(JSON.parse(body)))
-    .catch(async resp => errcb(await resp.text()))
+api = async (route, body, cb=x=>x, errcb=log) => {
+  const resp = await fetch('/api/'+route, 
+    {method: 'POST', headers: {body: JSON.stringify(body)}})
+  body = [...resp.headers].find(([k])=>k=='body')
+  if (body) cb(JSON.parse(body[1]))
+  else errcb(await resp.text())
+}
 
 submit.onclick = e => {
   e.preventDefault()
-  api('feedback/gives', {msg: msg.value, title: title.value},
+  api('feedback/give', {msg: msg.value, title: title.value},
     answer => (msg.value = title.value = '', log(answer)))
 }
 
