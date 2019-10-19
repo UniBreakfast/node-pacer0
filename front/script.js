50c1d777cce01c27440897f440eabfc3fccb2f81
UniBreakfast <unibreakfast1@gmail.com>
2019-10-19 21:39:53 +0300

    shorter err response handling

--- front/script.js
+++ front/script.js
@@ -1,18 +1,18 @@
 
 log('Page loaded')
 
-window.api = (route, body, cb=x=>x) => fetch('/api/'+route, 
+window.api = (route, body, cb=x=>x, errcb=log) => fetch('/api/'+route, 
   {method: 'POST', headers: {body: JSON.stringify(body)}})
     .then(resp => {
-      try { return [...resp.headers].find(([k])=>k=='body')[1] }
-      catch { throw resp.text() }
+      const body = [...resp.headers].find(([k])=>k=='body')
+      if (body) return body[1];  else throw resp
     })
     .then(body => cb(JSON.parse(body)))
-    .catch(answer => answer.then(err => cb(null, err)))
+    .catch(async resp => errcb(await resp.text()))
 
 submit.onclick = e => {
   e.preventDefault()
   api('feedback/gives', {msg: msg.value, title: title.value},
-    (answer, err) => (msg.value = title.value = '', log(answer || err)))
+    answer => (msg.value = title.value = '', log(answer)))
 }
 
