e0c332901e36594ff3bc83485706d2c0c345be99
UniBreakfast <unibreakfast1@gmail.com>
2019-10-23 22:33:35 +0300

    no empty feedbacks

--- front/submit.js
+++ front/submit.js
@@ -2,6 +2,7 @@
 log('Page loaded')
 
 submit.onclick = e => { e.preventDefault()
+  if (!msg.value && !title.value) return
   api('feedback/give', {msg: msg.value, title: title.value, author: author.value},
     answer => (msg.value = title.value = author.value = '', log(answer)))
 }
